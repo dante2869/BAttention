15fd595cfc9f634be792cf19b646ab2e3a09f59e
oscarArellano <oscardanielarellano@gmail.com>
2014-10-16 09:46:27 -0500

    Crear Juego Flechas

---
+++ Sln/Controllers/JuegoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sln.Controllers
+{
+    public class JuegoController : Controller
+    {
+        public ActionResult Juego()
+        {
+            return View();
+        }
+	}
+}
