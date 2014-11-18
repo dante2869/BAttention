be368b92130285b54dd87659105fcbbaa2b073cc
dante2869 <oscardanielarellano@gmail.com>
2014-11-18 00:24:26 -0500

    Creacion de control de seguridad

---
+++ Sln/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Sln.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sln.Controllers
+{
+    public class UserController : Controller
+    {
+        private EntidadSistema db = new EntidadSistema();
+
+        [HttpPost]
+        public ActionResult Login(string user, string psw)
+        {
+            var usuario = db.Usuarios.Where(p => p.Usuario1 == user && p.Clave == psw).SingleOrDefault();
+            if (usuario != null)
+            {
+                Session["UsuarioLogin"] = usuario;
+                Session["UsuarioNombre"] = usuario.Nombre.ToUpper() + " " + usuario.Apellido.ToUpper();
+                Session["UsuarioRol"] = usuario.IdRol;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logoff()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+        
+    }
+}
